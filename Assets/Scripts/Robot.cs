82c34a32cb06ee3f2d181cf3c70aa65656ba1e1f
ZakaryS <zak561@hotmail.com>
2022-11-04 20:35:31 -0400

    Chapter 10 - Animating the Robots

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -10,7 +10,10 @@ public class Robot : MonoBehaviour
     public int range;
     public float fireRate;
     public Transform missileFireSpot;
+    public Animator robot;
+
     UnityEngine.AI.NavMeshAgent agent;
+
     private Transform player;
     private float timeLastFired;
     private bool isDead;
@@ -45,7 +48,7 @@ public class Robot : MonoBehaviour
 
     private void fire()
     {
-        Debug.Log("Fire");
+        robot.Play("Fire");
     }
 
 }
