50afdc2ce5702b4b95b7a112a33e03a08ffa2a35
ZakaryS <zak561@hotmail.com>
2022-11-04 20:19:09 -0400

    Chapter 9 - Finished

---
+++ Assets/Scripts/Ammo.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Ammo : MonoBehaviour
+{
+    [SerializeField]
+    GameUI gameUI;
+
+    [SerializeField]
+    private int pistolAmmo = 20;
+    [SerializeField]
+    private int shotgunAmmo = 10;
+    [SerializeField]
+    private int assaultRifleAmmo = 50;
+
+    public Dictionary<string, int> tagToAmmo;
+
+    void Awake()
+    {
+        tagToAmmo = new Dictionary<string, int>
+        {
+            { Constants.Pistol , pistolAmmo },
+            { Constants.Shotgun , shotgunAmmo },
+            { Constants.AssaultRifle , assaultRifleAmmo },
+        };
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddAmmo(string tag, int ammo)
+    {
+        if (!tagToAmmo.ContainsKey(tag))
+        {
+            Debug.LogError("Unrecognized gun type passed: " + tag);
+        }
+        tagToAmmo[tag] += ammo;
+    }
+
+    public bool HasAmmo(string tag)
+    {
+        if (!tagToAmmo.ContainsKey(tag))
+        {
+            Debug.LogError("Unrecognized gun type passed: " + tag);
+        }
+        return tagToAmmo[tag] > 0;
+    }
+
+    public int GetAmmo(string tag)
+    {
+        if (!tagToAmmo.ContainsKey(tag))
+        {
+            Debug.LogError("Unrecognized gun type passed:" + tag);
+        }
+        return tagToAmmo[tag];
+    }
+
+    public void ConsumeAmmo(string tag)
+    {
+        if (!tagToAmmo.ContainsKey(tag))
+        {
+            Debug.LogError("Unrecognized gun type passed:" + tag);
+        }
+        tagToAmmo[tag]--;
+    }
+}
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float fireRate;
+    public Ammo ammo;
+    public AudioClip liveFire;
+    public AudioClip dryFire;
+
+    protected float lastFireTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastFireTime = Time.time - 10;
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+    }
+
+    protected void Fire()
+    {
+        if (ammo.HasAmmo(tag))
+        {
+            GetComponent<AudioSource>().PlayOneShot(liveFire);
+            ammo.ConsumeAmmo(tag);
+        }
+        else
+        {
+            GetComponent<AudioSource>().PlayOneShot(dryFire);
+        }
+        GetComponentInChildren<Animator>().Play("Fire");
+    }
+}
